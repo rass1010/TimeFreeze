77d29d70ecc3349f1cec01b71c8bbdc00a3aa56a
rass1010 <62247959+rass1010@users.noreply.github.com>
2022-04-17 17:09:50 +0200

    Add files via upload

---
+++ TimeFreeze/PluginInfo.cs
@@ -0,0 +1,10 @@
+﻿
+namespace TimeFreeze
+{
+    class PluginInfo
+    {
+        public const string GUID = "com.rass1010.gorillatag.timefreeze";
+        public const string Name = "TimeFreeze";
+        public const string Version = "1.0.0";
+    }
+}
